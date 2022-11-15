347f0797c49593f8209e84e39c18c9cfe7c5e622
hawonjungo <hawon.jungo@gmail.com>
2022-11-15 19:42:28 +1100

    Update UI

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -6,30 +6,35 @@ import {
   FaYoutube,
   FaFacebookSquare,
 } from "react-icons/fa";
+// import "./magnetic.js";
 
 const Footer = () => {
   return (
-    <div class="socialBox">
-      {" "}
-      {/* --partial magnetic-- */}
-      <div class="grid grid-cols-4 gap-4 social">
-        <button
-          onclick="location.href='https://www.facebook.com/junsubarashii'"
-          target="_blank"
-          type="button"
-        >
-          <FaFacebookSquare />
-        </button>
-        <button>
-          <FaTwitterSquare />
-        </button>
-        <button>
-          <FaInstagramSquare />
-        </button>
-        <button>
-          <FaYoutube />
-        </button>
+    <div class="cover">
+      <div class="socialBox">
+        {" "}
+        {/* --partial magnetic-- */}
+        <div class="grid grid-cols-4  social ">
+          <button
+            onclick="location.href='https://www.facebook.com/junsubarashii'"
+            target="_blank"
+            type="button"
+          >
+            <FaFacebookSquare />
+          </button>
+          <button>
+            <FaTwitterSquare />
+          </button>
+          <button>
+            <FaInstagramSquare />
+          </button>
+          <button>
+            <FaYoutube />
+          </button>
+        </div>
       </div>
+      {/* <div class="cursor cursor--large"></div>
+      <div class="cursor cursor--small"></div> */}
     </div>
   );
 };
--- src/layouts/main/Main.js
+++ src/layouts/main/Main.js
@@ -7,37 +7,45 @@ import avatar from "../../img/n.jpg";
 
 const Main = () => {
   return (
-    <div class="container bg-white">
-      <div class="welcome">
-        {" "}
-        <h1 className="text-3xl font-bold underline text-white ">
-          Ready to work!
-        </h1>
-      </div>
-      <div class="profile">
-        <div class="flex flex-row box">
-          <div class="basis-1/2 avatar">
-            <img class="img-fluid" src={avatar} alt="avatar" />
-          </div>
-          <div class="basis-1/2 info">
-            <p class="sayHi"> Hello</p>
-            <div class="name">
-              <p>
-                <span>I'm</span> Jun Hoang
+    <div class="body">
+      <div class="container">
+        <div class="welcome">
+          {" "}
+          <h1 className="text-3xl font-bold underline">Ready to work!</h1>
+        </div>
+        <div class="profile">
+          <div class="box">
+            <div class="avatar">
+              <img class="img-fluid" src={avatar} alt="avatar" />
+            </div>
+            <div class="info">
+              <p class="sayHi"> Hello</p>
+              <div class="name">
+                <p>
+                  <span>I'm</span> Jun Hoang
+                </p>
+              </div>
+              <p class="iconInfo">
+                <span>
+                  <FaGlobeAmericas />
+                </span>
+                Entry Mobile app & Web development
               </p>
+              <div class="detailInfo">
+                <p>___________________________________________</p>
+                <p>
+                  Design, build and publish iOS and Android mobile apps from
+                  scratch and graduate with a portfolio of work:
+                </p>
+                <p>● C++ ● JavaScript ● ReactJS ● Java </p>
+                <p>
+                  Please take a look on some of my lastest works in portfolio
+                  page.
+                </p>
+              </div>
+              <div>Nationality: Vietnamese</div>
+              <div>Address: Sydney, Australia</div>
             </div>
-            <p style={{ display: "inline-block" }}>
-              <FaGlobeAmericas />
-              Entry Mobile app & Web development
-            </p>
-            <div class="detailInfo">
-              <p>1</p>
-              <p>2</p>
-              <p>3</p>
-              <p>4</p>
-            </div>
-            <div>Nationality: Vietnamese</div>
-            <div>Address: Sydney, Australia</div>
           </div>
         </div>
       </div>
